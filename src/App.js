f85bcfc8f6521cae01b7ae05d763df0910218691
Dipanshugarg65 <118041289+Dipanshugarg65@users.noreply.github.com>
2023-05-05 21:19:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { Button } from '@chakra-ui/react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+function App() {
+  return (
+  <Router>
+
+<div>hello world </div>
+<Button> Click</Button>
+<Routes>
+  <Route/>
+</Routes>
+  </Router>
+   );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+// https://meet.google.com/fqn-regx-yso
